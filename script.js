67b22441864ed28e4dcd99ee83b43d66adaff7ad
Rotha Sam <samrotha98@gmail.com>
2023-12-11 16:03:32 +0700

    My First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Header Navigation
+// $(document).ready(function(){
+//     $(".fa-bars").click(function(){
+//         $(".nav-list").slideToggle();
+        
+//     });
+// });
+    
+
+$(document).ready(function(){
+    $("#toggleButton").click(function(){
+        $(".nav-list").toggleClass("active");
+    });
+});
+
+
+
+
+
